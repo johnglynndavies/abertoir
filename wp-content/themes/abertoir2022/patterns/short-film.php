cb8380fc4c3a79bb3fb60f6f280f68dc7910ea55
john.d <john.d@watershed.co.uk>
2023-09-21 16:25:33 +0100

    theme: add shortlist and short film block patterns and other style tweaks

---
+++ wp-content/themes/abertoir2022/patterns/short-film.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Title: Short film
+* Slug: abertoir2022/short-film
+* Categories: abertoir
+*/
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"isStackedOnMobile":false,"className":"aber-short-film"} -->
+<div class="wp-block-columns is-not-stacked-on-mobile aber-short-film"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"id":"","aspectRatio":"2/3","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Title</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Dir. Name<br>Country, 0000, 0mins</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Synopsis...</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:boldblocks/icon-separator {"icon":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 width=\u002246\u0022 height=\u002243\u0022 viewBox=\u00220 0 46 43\u0022\u003e\n  \u003cg id=\u0022Group_1\u0022 data-name=\u0022Group 1\u0022 transform=\u0022translate(-848 -221)\u0022\u003e\n    \u003cpath id=\u0022Union_1\u0022 data-name=\u0022Union 1\u0022 d=\u0022M97,464a8,8,0,0,1-8-8v-1a18.041,18.041,0,0,1-8-15v-1a18,18,0,0,1,18-18h10a18,18,0,0,1,18,18v1a18.041,18.041,0,0,1-8,15v1a8,8,0,0,1-8,8Zm14-10v6a4,4,0,0,0,4-4v-3l2-1a14.421,14.421,0,0,0,6-12v-1a13.671,13.671,0,0,0-14-14H99a13.671,13.671,0,0,0-14,14v1a14.422,14.422,0,0,0,6,12l2,1v3a4,4,0,0,0,4,4h2v-6a2,2,0,0,1,4,0v6h4v-6a2,2,0,0,1,4,0Zm-3-11a4,4,0,1,1,4,4A4,4,0,0,1,108,443Zm-15,0a4,4,0,1,1,4,4A4,4,0,0,1,93,443Z\u0022 transform=\u0022translate(767 -200)\u0022\u003e\u003c/path\u003e\n  \u003c/g\u003e\n\u003c/svg\u003e\n","width":{"sm":{"inherit":"lg"},"md":{"inherit":"lg"},"lg":{"value":{"width":"33.33333%","value":"33.33333%"},"inherit":null}},"margin":{"sm":{"inherit":"lg"},"md":{"inherit":"lg"},"lg":{"value":{"top":"2rem","bottom":"4rem"},"inherit":null}}} -->
+<div class="wp-block-boldblocks-icon-separator" style="--bb--margin-top--sm:var(--bb--margin-top--lg);--bb--margin-bottom--sm:var(--bb--margin-bottom--lg);--bb--margin-top--md:var(--bb--margin-top--lg);--bb--margin-bottom--md:var(--bb--margin-bottom--lg);--bb--margin-top--lg:2rem;--bb--margin-bottom--lg:4rem" role="separator"><div class="wp-block-boldblocks-icon-separator__inner icon-center is-align-center" style="--bb--width--sm:var(--bb--width--lg);--bb--width--md:var(--bb--width--lg);--bb--width--lg:33.33333%;--bb-border:1px solid;--bb--icon-width:1em;--bb--icon-spacing:.5em"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="43" viewBox="0 0 46 43">
+  <g id="Group_1" data-name="Group 1" transform="translate(-848 -221)">
+    <path id="Union_1" data-name="Union 1" d="M97,464a8,8,0,0,1-8-8v-1a18.041,18.041,0,0,1-8-15v-1a18,18,0,0,1,18-18h10a18,18,0,0,1,18,18v1a18.041,18.041,0,0,1-8,15v1a8,8,0,0,1-8,8Zm14-10v6a4,4,0,0,0,4-4v-3l2-1a14.421,14.421,0,0,0,6-12v-1a13.671,13.671,0,0,0-14-14H99a13.671,13.671,0,0,0-14,14v1a14.422,14.422,0,0,0,6,12l2,1v3a4,4,0,0,0,4,4h2v-6a2,2,0,0,1,4,0v6h4v-6a2,2,0,0,1,4,0Zm-3-11a4,4,0,1,1,4,4A4,4,0,0,1,108,443Zm-15,0a4,4,0,1,1,4,4A4,4,0,0,1,93,443Z" transform="translate(767 -200)"></path>
+  </g>
+</svg></div></div>
+<!-- /wp:boldblocks/icon-separator --></div>
+<!-- /wp:group -->
